12b376282035b962dd43fb7bae49da510a45fad5
Ramen Tabetarou <kobeengineer1225@gmail.com>
2022-07-23 16:18:29 +0900

    react-route-dom v6
    Signed-off-by: Ramen Tabetarou <kobeengineer1225@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import Login from "./features/login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />}></Route>
+                    <Route path="/tasks" element={<App />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
